e8277bbdd240bc8a415b075153f7c02cbc77f60c
svetlana <masterica75@mail.ru>
2023-03-14 21:25:21 +0700

    задача №26

---
+++ homework/5/1.py
@@ -0,0 +1,25 @@
+# Задача 26:  Напишите программу, которая на вход принимает два числа A и B,
+# и возводит число А в целую степень B с помощью рекурсии.
+
+# *Пример:*
+
+# A = 3; B = 5 -> 243 (3⁵)
+#     A = 2; B = 3 -> 8
+
+
+import random
+
+number_1 = int(input('Введите первое число: '))
+number_2 = int(input('Введите второе число: '))
+
+
+# def degree_number(a, b):
+#     print(number_1**number_2)
+# degree_number(number_1, number_2)
+
+
+def degree_number(a, b):
+    return number_1**number_2
+
+
+print(degree_number(number_1, number_2))
