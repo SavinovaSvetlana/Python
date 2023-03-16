4d38e6fb0485a57c5ae0d5f83a9a3bf214ce02c8
svetlana <masterica75@mail.ru>
2023-03-16 13:50:18 +0700

    задача из лекции

---
+++ seminars/lekciy/1.py
@@ -0,0 +1,18 @@
+# 1. В списке хранятся числа. Нужно выбрать только чётные числа и составить список пар (число; квадрат числа).
+# Пример: 1 2 3 5 8 15 23 38
+# Получить: [(2, 4), (8, 64), (38, 1444)]
+
+
+import random
+
+my_list = int(input('Введите количество элементов первого списка: '))
+my_list_one = [random.randint(0, 10) for _ in range(my_list)]
+print(my_list_one)
+temp_list = []
+
+for i in my_list_one:
+    if i % 2 == 0:
+        temp_list.append((i, i ** 2))
+
+
+print(temp_list)
