1a7c529eab9faf657ef20a99f2ef2b7ec5cdbb13
svetlana <masterica75@mail.ru>
2023-03-16 17:31:43 +0700

    задача из лекции

--- seminars/lekciy/3.py
+++ seminars/lekciy/3.py
@@ -4,3 +4,16 @@ data = [13, 15, 98, 175, 5, 45]
 print(data)
 res = list(filter(lambda x: x % 10 == 5, data))
 print(res)
+
+
+# Функция зип
+users = ['user1', 'user2', 'user3']
+ids = [3, 8, 56]
+data = list(zip(users, ids))
+print(data)
+
+
+# Функция енумерайт
+users = ['user1', 'user2', 'user3']
+data = list(enumerate(users))
+print(data)
